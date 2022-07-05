2e1a4fcdf67ecc8c8ed779052ee496827c5542b1
Sorokina Arina <arinos1993@gmail.com>
2022-07-05 18:29:21 +0300

    feat(homework_6):do homework

---
+++ frontend/homework/homework_8/script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// function makeFibonacciFunction () {
+//     const Fib = 0;
+//     const Fib2 = 1
+
+// }
+let i;
+
+for (i = 0; i < 5; i++ ) {
+    console.log('Fibonacci', i)
+}
